84cc0fa51a2eb62676247afe6a2916c68da4109b
Elliot Cardona <elliot.cardona@upr.edu>
2023-03-17 02:39:06 -0400

    added left side of ui

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,38 +1,63 @@
 
 
 import Layout from '@/components/Layout';
-import { Button } from '@nextui-org/react';
+import EButton from '@/components/eButton';
 import Link from 'next/link';
 
-
+const uname = "Bruce Wayne"
+const rating = "4"
+const joined = "2022"
+const location = "Moca, PR"
+const languages = ["Spanish", "English"]
 const Profile = () => {
     return (
         <Layout>
 
             <main className="grid grid-cols-5 gap-4 py-24">
-                <div className='grid grid-rows-5 gap-2 '>
+                <div className='grid  gap-4 pl-4 '>
 
-                    <div className=' flex items-center justify-center ' >
-                    <Link className="rounded-md bg-gray-500 px-16 py-5  text-sm font-semibold text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-                            href="/">Dashboard</Link>
-                        
+                    
+                    <div className="flex justify-center text-center" >
+                        <div
+                            className="block min-w-full rounded-xl pt-4 bg-white ring-stone-200 ring-1 shadow-lg dark:bg-neutral-700 ">
+                            <a href="#!" className=''>
+                                <img
+                                    className="h-40  mask mask-circle "
+                                    src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
+                                    alt="" />
+                            </a>
+                            <div className="p-6">
+                                <h5
+                                    className="mb-2 text-2xl font-bold leading-tight text-neutral-800 dark:text-neutral-50">
+                                    {uname}
+                                </h5>
+                                <p className=" font-semibold text-neutral-600 dark:text-neutral-200 ">
+                                    Rating: {rating}/5
+                                </p>
+
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex items-center justify-center'>
-                    <Link className="rounded-md  bg-gray-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-                            href="/">Messages</Link>
+                    <div className="flex justify-center">
+                        <div
+                            className="block min-w-full rounded-lg bg-white ring-1 ring-stone-200 shadow-lg dark:bg-neutral-700">
+
+                            <div className="p-6">
+                                <h5
+                                    className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                                    Details:
+                                </h5>
+                                <ul>
+                                    <li>Joined in {joined}</li>
+                                    <li>Lives in {location}</li>
+                                    <li>Speaks {languages[0]}, {languages[1]}</li>
+                                </ul>
+                                
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex items-center justify-center'>
-                    <Link className="rounded-md  bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-                            href="/">Listings</Link>
-                    </div>
-                    <div className='flex items-center justify-center'>
-                    <Link className="rounded-md  bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-                            href="/">Contracts</Link>
-                    </div>
-                    <div className='flex items-center justify-center'>
-                    <Link className="rounded-md  bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-                            href="/">Account Settings</Link>
-                    </div>
+
+
 
 
 
@@ -40,8 +65,8 @@ const Profile = () => {
 
 
                 </div>
-                <div className='text-center col-span-4'>
-                    <h1>Data</h1>
+                <div className='text-center col-span-4 pr-4 '>
+                    <div className='ring-1 ring-stone-200'><h1 >Data</h1></div>
                 </div>
             </main>
         </Layout>
