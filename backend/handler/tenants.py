c48b1f26fd78e6523ac8bfc231c0cd4c961bf4b7
DannyBoy <daniel.crespo1@upr.edu>
2023-03-23 19:46:23 -0400

    Validated input when creating/updating tenants

--- backend/handler/tenants.py
+++ backend/handler/tenants.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from backend.dao.tenants import Tenants
+import re
 
 class TenantHandler:
   def __init__(self):
@@ -74,12 +75,18 @@ class TenantHandler:
         return False, 'Empty Name'
       if not len(email.strip()):
         return False, 'Empty Email'
+      if self.emailValid(email):
+        return False, 'Enter Valid Email'
       if self.emailTaken(email, identifier):
         return False, 'Email Taken'
       if not len(password.strip()):
         return False, 'Empty Password'
+      if self.passwordValid(password):
+        return False, 'Password must contain 4-8 characters, at least one uppercase/lowercase letter, at least one digit, and no spaces. (Special characters are optional)'
       if not len(phone.strip()):
         return False, 'Empty Phone Number'
+      if self.phoneValid(phone):
+        return False, 'Enter Valid Phone Number'
       if self.phoneTaken(phone, identifier):
         return False, 'Phone Number Taken'
     except:
@@ -87,8 +94,20 @@ class TenantHandler:
     else:
       return True , ''
 
+  def emailValid(self, email):
+    emailRegex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    return not re.search(emailRegex, email)
+  
+  def passwordValid(self, password):
+    passwordRegex = '^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,8}$'
+    return not re.search(passwordRegex, password)
+
   def emailTaken(self, email, identifier):
     return self.tenants.getEmail(email, identifier)
   
+  def phoneValid(self, phone):
+    phoneRegex = '^[2-9]\d{2}-\d{3}-\d{4}$'
+    return not re.search(phoneRegex, phone)
+
   def phoneTaken(self, number, identifier):
     return self.tenants.getPhoneNumber(number, identifier)
