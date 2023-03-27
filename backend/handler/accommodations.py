204ad0fc363f4ea7afc7b9937f6e0904584a85fd
DannyBoy <daniel.crespo1@upr.edu>
2023-03-26 22:53:50 -0400

    Added input restrictions for accomodation

--- backend/handler/accommodations.py
+++ backend/handler/accommodations.py
@@ -1,11 +1,14 @@
 from flask import jsonify
 from backend.dao.accommodations import Accommodations
 from backend.dao.shared_amenities import SharedAmenities
+from backend.dao.landlords import Landlords
+import re
 
 class AccommodationHandler:
   def __init__(self):
     self.accommodations = Accommodations()
     self.amenities = SharedAmenities()
+    self.landlords = Landlords()
   
   def dictionary(self, row):
     data = {}
@@ -49,27 +52,120 @@ class AccommodationHandler:
       return jsonify('Accommodations Not Found'), 405
 
   def addAccommodation(self, json):
-    daoAccommodation = self.accommodations.addAccommodation(
-      json['accm_title'], json['accm_street'], json['accm_number'], 
-      json['accm_city'], json['accm_state'], json['accm_country'], 
-      json['accm_zipcode'], json['accm_description'], json['landlord_id'])
-
-    if daoAccommodation:
-      daoAmenities = self.amenities.addSharedAmenities(daoAccommodation[0])
+    title = json['accm_title']
+    street = json['accm_street']
+    number = json['accm_number']
+    city = json['accm_city']
+    state = json['accm_state']
+    country = json['accm_country']
+    zip = json['accm_zipcode']
+    description = json['accm_description']
+    landlordID = json['landlord_id']
+    valid, reason = self.checkLandlordID(landlordID)
+    if not valid:
+      return jsonify(reason), 400
+    valid, reason = self.checkInput(title, street, number, city, state, country, zip)
+    # add accommodation if input is valid
+    if valid:
+      newAccommodation = self.accommodations.addAccommodation(title, street, number, city, 
+                                                              state, country, zip, description, landlordID)
+      if newAccommodation:
+        daoAmenities = self.amenities.addSharedAmenities(newAccommodation[0])
+      else:
+        return jsonify('Error adding Accommodation'), 405
       if daoAmenities:
-        return jsonify(self.dictionary(daoAccommodation)), 200
+        return jsonify(self.dictionary(newAccommodation)), 200
       else:
         jsonify('Error adding Shared Amenities to Accommodation'), 405
     else:
-      return jsonify('Error adding Accommodation'), 405
+      # returns reason why input was invalid
+      return jsonify(reason), 400
 
   def updateAccommodation(self, json):
-    daoAccommodation = self.accommodations.updateAccommodation(
-      json['accm_id'], json['accm_title'], json['accm_street'], 
-      json['accm_number'], json['accm_city'], json['accm_state'], 
-      json['accm_country'], json['accm_zipcode'], json['accm_description'])
-
-    if daoAccommodation:
-      return jsonify(self.dictionary(daoAccommodation)), 200
+    accm_id = json['accm_id']
+    title = json['accm_title']
+    street = json['accm_street']
+    number = json['accm_number']
+    city = json['accm_city']
+    state = json['accm_state']
+    country = json['accm_country']
+    zip = json['accm_zipcode']
+    description = json['accm_description']
+    valid, reason = self.checkAccmID(accm_id)
+    if not valid:
+      return jsonify(reason), 400
+    valid, reason = self.checkInput(title, street, number, city, state, country, zip)
+    # add accommodation if input is valid
+    if valid:
+      updatedAccommodation = self.accommodations.updateAccommodation(accm_id, title, street, number, city, 
+                                                              state, country, zip, description)
+      if updatedAccommodation:
+        return jsonify(self.dictionary(updatedAccommodation)), 200
+      else:
+        return jsonify('Error updating Accommodation'), 405
     else:
-      return jsonify('Error updating Accommodation'), 405
+      # returns reason why input was invalid
+      return jsonify(reason), 400
+    
+  def checkInput(self, title, street, number, city, state, country, zip):
+    # strip function removes any spaces given
+    try:
+      if not len(title.strip()):
+        return False, 'Empty Title'
+      if not len(street.strip()):
+        return False, 'Empty Street'
+      if self.accomNumValid(number):
+        return False, 'Accommodation number can only contain numbers, leters and hyphen. (Hyphen are optional but cannot start or end with a hyphen -)'
+      if not len(city.strip()):
+        return False, 'Empty City'
+      if self.onlyCharacters(city):
+        return False, 'City cannot contain numbers or special characters.'
+      if self.onlyCharacters(state):
+        return False, 'State cannot contain numbers or special characters.'
+      if not len(country.strip()):
+        return False, 'Empty Country'
+      if self.onlyCharacters(country):
+        return False, 'Country cannot contain numbers or special characters.'
+      if not len(zip.strip()):
+        return False, 'Empty Zip Code'
+      if self.zipValid(zip):
+        return False, 'Enter a Valid Zip Code'
+    except:
+      return False, 'Invalid Input'
+    else:
+      return True , ''
+
+  def checkLandlordID(self, landlordID):
+    try:
+      if not self.landlords.getById(landlordID):
+        return False, 'Landlord Not Found'
+    except:
+      return False, 'Invalid Input'
+    else:
+      return True , ''
+
+  def checkAccmID(self, accmID):
+    try:
+      if not self.accommodations.getById(accmID):
+        return False, 'Accommodation Not Found'
+    except:
+      return False, 'Invalid Input'
+    else:
+      return True , ''
+
+  def zipValid(self, zip):
+    zipRegex = '^\d{5}'
+    return not re.search(zipRegex, zip)
+    
+  def accomNumValid(self, number):
+    if not number:
+      return
+    numberRegex = '^(?!-)(?!.*-$)[\w-]{1,10}$'
+    return not re.search(numberRegex, number)
+
+  def onlyCharacters(self, string):
+    if not string:
+      return
+    stringRegex = '^[a-zA-Z ]{1,20}$'
+    return not re.search(stringRegex, string)
+  
