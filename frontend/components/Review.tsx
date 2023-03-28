f42fd90f7faee191a71e2c7c157647d47b7f9dfc
Elliot Cardona <elliot.cardona@upr.edu>
2023-03-28 18:42:47 -0400

    added links to review items

--- frontend/components/Review.tsx
+++ frontend/components/Review.tsx
@@ -9,8 +9,7 @@ const Review: React.FC<Props> = ({ }) => {
     return (
         <div className='flex flex-row ring-1 shadow-lg ring-stone-200 rounded-md mt-4 mb-2'>
             <div className='flex flex-col flex-auto '>
-                <h1 className='pt-6 pl-6 pr-6 pb-2 text-2xl font-bold text-left'>2BR Condo ON THE BEACH! Restaurant- Pool- Hot Tub!
-</h1>
+                <h1 className='pt-6 pl-6 pr-6 pb-2 text-2xl font-bold text-left'><a href='/' className='hover:underline'>2BR Condo ON THE BEACH! Restaurant- Pool- Hot Tub!</a></h1>
                 <div className='flex flex-row flex-nowrap  w-full'>
                     <div className='flex-auto '>
                         <p className=' text-lg  pl-8'>Had a great time and the place was great. The beach was beautiful and the place had everything we needed for a terrific vacation.</p>
@@ -21,13 +20,13 @@ const Review: React.FC<Props> = ({ }) => {
                 <div className="flex flex-nowrap flex-row  items-center pl-2 pb-2">
                     <div className="items-center">
                         <div className="avatar p-4">
-                            <div className=" w-10 rounded-full ring-1 ring-accent ring-offset-base-100 ring-offset-2">
-                                <img className='aspect-square' src="/images/person.png" />
+                            <div className=" w-10 rounded-full ring-1 ring-accent ring-offset-base-100 ring-offset-2 hover:shadow-lg hover:ring-2">
+                                <a href='' className=''><img className='aspect-square' src="/images/person.png" /></a>
                             </div>
                         </div>
                     </div>
                     <div className='flex flex-col'>
-                        <h1 className='font-semibold'>Maya Williams</h1>
+                        <h1 className='font-semibold'><a href='/' className='hover:underline'>Maya Williams</a></h1>
                         <h2 className='text-neutral-600'>March 2022</h2>
                     </div>
 
@@ -35,7 +34,7 @@ const Review: React.FC<Props> = ({ }) => {
             </div>
             <div className=' m-auto  items-center'>
                 <div className=" w-60 p-6 ">
-                    <img className='aspect-video rounded-lg' src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg" />
+                    <a href='/'><img className='aspect-video rounded-lg hover:shadow-lg hover:ring-1 hover:ring-stone-200' src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg" /></a>
                 </div>
             </div>
         </div>
