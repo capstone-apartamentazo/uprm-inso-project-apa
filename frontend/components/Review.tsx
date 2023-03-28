8ce407cbd8a0badca6fb480c4651279948d0031a
Elliot Cardona <elliot.cardona@upr.edu>
2023-03-28 19:40:14 -0400

    Made Reviews values variables

--- frontend/components/Review.tsx
+++ frontend/components/Review.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 
 
 type Props = {
+    opinion: string;
+    listingTitle: string; 
+    listingImg:string;
+    name: string;
+    date: string;
+    userImg: string;
 
 }
 
-const Review: React.FC<Props> = ({ }) => {
+const Review: React.FC<Props> = ({ opinion, listingTitle, listingImg, name, date, userImg }) => {
     return (
         <div className='flex flex-row ring-1 shadow-lg ring-stone-200 rounded-md mt-4 mb-2'>
             <div className='flex flex-col flex-auto '>
-                <h1 className='pt-6 pl-6 pr-6 pb-2 text-2xl font-bold text-left'><a href='/' className='hover:underline'>2BR Condo ON THE BEACH! Restaurant- Pool- Hot Tub!</a></h1>
+                <h1 className='pt-6 pl-6 pr-6 pb-2 text-2xl font-bold text-left'><a href='/' className='hover:underline'>{listingTitle}</a></h1>
                 <div className='flex flex-row flex-nowrap  w-full'>
                     <div className='flex-auto '>
-                        <p className=' text-lg  pl-8'>Had a great time and the place was great. The beach was beautiful and the place had everything we needed for a terrific vacation.</p>
+                        <p className=' text-lg  pl-8'>{opinion}</p>
                     </div>
 
                 </div>
@@ -21,20 +27,20 @@ const Review: React.FC<Props> = ({ }) => {
                     <div className="items-center">
                         <div className="avatar p-4">
                             <div className=" w-10 rounded-full ring-1 ring-accent ring-offset-base-100 ring-offset-2 hover:shadow-lg hover:ring-2">
-                                <a href='' className=''><img className='aspect-square' src="/images/person.png" /></a>
+                                <a href='' className=''><img className='aspect-square' src={userImg} /></a>
                             </div>
                         </div>
                     </div>
                     <div className='flex flex-col'>
-                        <h1 className='font-semibold'><a href='/' className='hover:underline'>Maya Williams</a></h1>
-                        <h2 className='text-neutral-600'>March 2022</h2>
+                        <h1 className='font-semibold'><a href='/' className='hover:underline'>{name}</a></h1>
+                        <h2 className='text-neutral-600'>{date}</h2>
                     </div>
 
                 </div>
             </div>
             <div className=' m-auto  items-center'>
                 <div className=" w-60 p-6 ">
-                    <a href='/'><img className='aspect-video rounded-lg hover:shadow-lg hover:ring-1 hover:ring-stone-200' src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg" /></a>
+                    <a href='/'><img className='aspect-video rounded-lg hover:shadow-lg hover:ring-1 hover:ring-stone-200' src={listingImg} /></a>
                 </div>
             </div>
         </div>
