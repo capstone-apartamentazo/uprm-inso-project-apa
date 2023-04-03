d4aa655b8c19bb494cd31b2aae177bbe8325fa92
Elliot Cardona <elliot.cardona@upr.edu>
2023-04-02 21:48:44 -0400

    Changed url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	publicRuntimeConfig: {
 		site: {
 			name: 'Apartamentazo',
-			url: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://apartamentazo.com/',
+			url: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://apartamentazo.herokuapp.com',
 			title: 'Apartamentazo',
 			description: 'Helping you find your next college apartment',
 			socialPreview: '/images/preview.png',
