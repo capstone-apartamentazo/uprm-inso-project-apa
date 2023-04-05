4a8316da9388cdccdda30aace18b8414cdffb2bf
carlostorres62 <carlos.torres62@upr.edu>
2023-04-04 21:19:51 -0400

    a

--- frontend/pages/loaderio-fa0db6d49f8bf2f6c766b1e054e189b8.tsx
+++ frontend/pages/loaderio-fa0db6d49f8bf2f6c766b1e054e189b8.tsx
@@ -1,17 +1,13 @@
 import getConfig from 'next/config';
 
-import Layout from '@/components/Layout';
-
 const { publicRuntimeConfig } = getConfig();
 const { name } = publicRuntimeConfig.site;
 
 const Loaderio = () => {
 	return (
-		<Layout>
-				<div>
-          loaderio-fa0db6d49f8bf2f6c766b1e054e189b8
-				</div>
-		</Layout>
+    <div>
+      loaderio-fa0db6d49f8bf2f6c766b1e054e189b8
+    </div>
 	);
 };
 
