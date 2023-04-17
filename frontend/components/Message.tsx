6767df7befa166c550e8f578a2ce0d68ec15ca41
Elliot Cardona <elliot.cardona@upr.edu>
2023-04-16 23:56:31 -0400

    added click features to message list

--- frontend/components/Message.tsx
+++ frontend/components/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 
 type Props = {
@@ -10,10 +11,14 @@ type Props = {
 
 }
 
+
 const Message: React.FC<Props> = ({ userName, userImg, body, date, read }) => {
+    const [iread, setRead] = useState(read);
+
+
     return (
 
-        <div className="flex flex-nowrap flex-row  items-center pl-2 pb-2 ">
+        <div onClick={() => setRead(true)} className="flex flex-nowrap flex-row rounded-md  items-center  cursor-pointer pl-2 pb-2 mx-2 my-2  hover:bg-gray-100">
             <div className="items-center">
                 <div className="avatar p-4">
                     <div className=" w-10 rounded-full ring-1 ring-accent ring-offset-base-100 ring-offset-2 hover:shadow-lg hover:ring-2">
@@ -24,13 +29,13 @@ const Message: React.FC<Props> = ({ userName, userImg, body, date, read }) => {
             </div>
             <div className='flex flex-col items-start'>
                 <div className='flex flex-row'>
-                    <h1 className={''+(read ? 'font-semibold' : 'font-bold')}>{userName}</h1>
+                    <h1 className={'' + (iread ? 'font-semibold' : 'font-bold')}>{userName}</h1>
                     <h1 className='text-black font-extrabold px-1'>·</h1>
-                    <h1 className={''+(read ? 'text-neutral-600 pr-1 font-normal' : 'text-neutral-600 pr-1 font-semibold')}>{date}</h1>
-                    <h1 className='items-center'><span className={" "+(read ? 'hidden':'badge badge-accent badge-sm text-white font-semibold')}>NEW</span></h1>
+                    <h1 className={'' + (iread ? 'text-neutral-600 pr-1 font-normal' : 'text-neutral-600 pr-2 font-semibold')}>{date}</h1>
+                    <h1 className='items-center'><span className={" " + (iread ? 'hidden' : 'badge badge-accent badge-sm text-white font-semibold')}>NEW</span></h1>
                 </div>
 
-                <h2 className={''+(read ? 'font-normal' : 'font-semibold')}>{body}</h2>
+                <h2 className={'' + (iread ? 'font-normal' : 'font-semibold')}>{body}</h2>
             </div>
 
         </div>
