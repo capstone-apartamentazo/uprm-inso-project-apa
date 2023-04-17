57621b64b15ba46899c5d29c9f5e1592c8c7cbce
Elliot Cardona <elliot.cardona@upr.edu>
2023-04-17 01:03:31 -0400

    just to save dif layouts

--- frontend/pages/messages.tsx
+++ frontend/pages/messages.tsx
@@ -29,38 +29,73 @@ const Messages = () => {
 
                 </div>
                 <div className='flex flex-col flex-initial flex-grow basis-1/4 mt-20 pl-4 mr-10 w-9/12 '>
-                    <div className='rounded-xl p-6 ring-1 ring-stone-200'>
-                        <div className="chat chat-start">
+
+                    <div className='rounded-xl p-6  ring-1 shadow-lg ring-stone-200'>
+                        <div className='border border-stone-200 w-full'>
                             <div className="chat-image avatar">
                                 <div className="w-10 rounded-full">
-                                    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                    <img src="/images/person.png" />
                                 </div>
-                            </div>
-                            <div className="chat-header">
-                                Obi-Wan Kenobi
-                                <time className="text-xs opacity-50">12:45</time>
-                            </div>
-                            <div className="chat-bubble chat-bubble-accent">You were the Chosen One!</div>
-                            <div className="chat-footer opacity-50">
-                                Delivered
-                            </div>
-                        </div>
-                        <div className="chat chat-end">
-                            <div className="chat-image avatar">
-                                <div className="w-10 rounded-full">
-                                    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                                </div>
-                            </div>
-                            <div className="chat-header">
-                                Anakin
-                                <time className="text-xs opacity-50">12:46</time>
-                            </div>
-                            <div className="chat-bubble chat-bubble-primary text-white">I hate you!</div>
-                            <div className="chat-footer opacity-50">
-                                Seen at 12:46
                             </div>
                         </div>
 
+
+
+                    </div>
+
+                    <div className="overflow-x-auto rounded-lg ring-1 ring-stone-200">
+                        <table className="table w-full ">
+                            {/* head */}
+                            <thead>
+                                <tr>
+
+                                    <th>Name</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* row 1 */}
+                                <tr>
+
+                                </tr>
+                                {/* row 2 */}
+                                <tr>
+                                    <div className="chat chat-start">
+                                        <div className="chat-image avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img src="/images/person.png" />
+                                            </div>
+                                        </div>
+                                        <div className="chat-header">
+                                            Obi-Wan Kenobi
+                                            <time className="text-xs opacity-50">12:45</time>
+                                        </div>
+                                        <div className="chat-bubble bg-stone-200 ">You were the Chosen One!</div>
+                                        <div className="chat-footer opacity-50">
+                                            Delivered
+                                        </div>
+                                    </div>
+                                </tr>
+                                {/* row 3 */}
+                                <tr>
+                                    <div className="chat chat-end">
+                                        <div className="chat-image avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img src="/images/person.png" />
+                                            </div>
+                                        </div>
+                                        <div className="chat-header">
+                                            Anakin
+                                            <time className="text-xs opacity-50">12:46</time>
+                                        </div>
+                                        <div className="chat-bubble chat-bubble-primary text-white">I hate you!</div>
+                                        <div className="chat-footer opacity-50">
+                                            Seen at 12:46
+                                        </div>
+                                    </div>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
 
 
