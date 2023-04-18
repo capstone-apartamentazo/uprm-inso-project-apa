9dfa88ef673cc89bbbb11b235c03e09ec68c6381
Elliot Cardona <elliot.cardona@upr.edu>
2023-04-17 23:14:00 -0400

    Changed Message for Conversation. Added axios dependency.

--- frontend/pages/messages.tsx
+++ frontend/pages/messages.tsx
@@ -1,7 +1,9 @@
 import Layout from '@/components/Layout';
 import Listing from '@/components/Listing';
-import Message from '@/components/Message';
+import Conversation from '@/components/Conversation';
 import Image from 'next/image';
+import { GetServerSideProps } from 'next'
+import axios from 'axios';
 
 const uname = 'Bruce Wayne';
 const rating = '4';
@@ -10,99 +12,105 @@ const location = 'Moca, PR';
 const languages = ['Spanish', 'English'];
 const reviewCount = 2;
 
-const Messages = () => {
+interface Message {
+    Content: string
+    "Landlord ID": number
+    "Landlord Sent Message": boolean
+    "Message ID": number
+    Read: boolean
+    "Send Date": string
+    "Tenant ID": number
+}
+
+interface Props {
+    messages: Message[]
+}
+
+
+const Messages: React.FC<Props> = ({ messages }) => {
     return (
         <Layout>
-            <main className='flex flex-row flex-nowrap pt-24'>
-                <div className='flex  flex-col flex-initial basis-1/4 pt-10 pl-6 pr-6 '>
-                    <h1 className=' text-3xl font-bold text-left  '>
-                        {' '}
-                        <a href='/' className='hover:underline'>
-                            Messages
-                        </a>
+            <main className='flex flex-col flex-nowrap mt-20 '>
+                <div className='mt-10 mb-4 pl-6 items-center'>
+                    <h1 className=' text-3xl font-bold text-left '>
+                        Messages
                     </h1>
-                    <div className='block min-w-full rounded-xl pt-2 bg-white ring-stone-200 ring-1 shadow-lg dark:bg-neutral-700 items-center text-center'>
-
-                        <Message userName='Elliot' userImg='/images/person.png' body='wsknwicw' date="2d ago" read={false}></Message>
-                        <Message userName='Marcos' userImg='/images/person.png' body='helloo' date="1m ago" read={true}></Message>
-                    </div>
-
                 </div>
-                <div className='flex flex-col flex-initial flex-grow basis-1/4 mt-20 pl-4 mr-10 w-9/12 '>
+                <div className='flex flex-row flex-nowrap'>
+                    <div className='flex  flex-col flex-initial basis-1/4 pt-4 pl-6 pr-6 '>
 
-                    <div className='rounded-xl p-6  ring-1 shadow-lg ring-stone-200'>
-                        <div className='border border-stone-200 w-full'>
-                            <div className="chat-image avatar">
-                                <div className="w-10 rounded-full">
-                                    <img src="/images/person.png" />
-                                </div>
-                            </div>
+                        <div className='block min-w-full rounded-xl pt-2 bg-white ring-stone-200 ring-1 shadow-lg dark:bg-neutral-700 items-center text-center'>
+
+                            <Conversation userName='Elliot' userImg='/images/person.png' body='wsknwicw' date="2d ago" read={false}></Conversation>
+                            <Conversation userName='Marcos' userImg='/images/person.png' body='helloo' date="1m ago" read={true}></Conversation>
                         </div>
 
-
-
                     </div>
+                    <div className='flex flex-col flex-initial flex-grow basis-1/4 mt-4 ml-2 mr-10 w-9/12 h-96 rounded-xl ring-1 ring-stone-200 shadow-lg overflow-hidden '>
+                        <div className='sticky top-0'>
+                            <div className='flex flex-row flex-nowrap bg-white   drop-shadow-md items-center '>
 
-                    <div className="overflow-x-auto rounded-lg ring-1 ring-stone-200">
-                        <table className="table w-full ">
-                            {/* head */}
-                            <thead>
-                                <tr>
+                                <div className="avatar p-4">
+                                    <div className=" w-10 rounded-full ring-1 ring-accent ring-offset-base-100 ring-offset-2 hover:shadow-lg hover:ring-2">
+                                        <a href='' className=''><img className='aspect-square' src='/images/person.png' /></a>
+                                    </div>
+                                </div>
 
-                                    <th>Name</th>
 
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {/* row 1 */}
-                                <tr>
+                                <h1 className='font-semibold text-xl'>{ messages.at(1)?.['Landlord Sent Message'] ? "messages.at(1)?.['Landlord ID'](Landlord Name)" : "messages.at(1)?.['Tenant ID'](Tenant Name)"}</h1>
+                            </div>
+                        </div>
+                        <div className='overflow-auto mx-4'>
 
-                                </tr>
-                                {/* row 2 */}
-                                <tr>
-                                    <div className="chat chat-start">
-                                        <div className="chat-image avatar">
-                                            <div className="w-10 rounded-full">
-                                                <img src="/images/person.png" />
-                                            </div>
-                                        </div>
-                                        <div className="chat-header">
-                                            Obi-Wan Kenobi
-                                            <time className="text-xs opacity-50">12:45</time>
-                                        </div>
-                                        <div className="chat-bubble bg-stone-200 ">You were the Chosen One!</div>
-                                        <div className="chat-footer opacity-50">
-                                            Delivered
+
+                            
+                                {messages.map((message, index) => (
+                                    <div key={message["Message ID"]}>
+                                    <div className={""+ (message['Landlord Sent Message'] ? "chat chat-start" : "chat chat-end")}>
+                                    <div className="chat-image avatar">
+                                        <div className="w-10 rounded-full">
+                                            <img src="/images/person.png" />
                                         </div>
                                     </div>
-                                </tr>
-                                {/* row 3 */}
-                                <tr>
-                                    <div className="chat chat-end">
-                                        <div className="chat-image avatar">
-                                            <div className="w-10 rounded-full">
-                                                <img src="/images/person.png" />
-                                            </div>
-                                        </div>
-                                        <div className="chat-header">
-                                            Anakin
-                                            <time className="text-xs opacity-50">12:46</time>
-                                        </div>
-                                        <div className="chat-bubble chat-bubble-primary text-white">I hate you!</div>
-                                        <div className="chat-footer opacity-50">
-                                            Seen at 12:46
-                                        </div>
+                                    <div className="chat-header">
+                                    {message['Landlord Sent Message'] ? "message[Landlord ID](Landlord Name)" : "message[Tenant ID](Tenant Name)"}
+                                        <time className="text-xs opacity-50">{message["Send Date"]}</time>
                                     </div>
-                                </tr>
-                            </tbody>
-                        </table>
+                                    <div className={""+(message['Landlord Sent Message'] ? "chat-bubble text-white chat-bubble-accent" : "chat-bubble text-white chat-bubble-primary")}>{message.Content}</div>
+                                    <div className="chat-footer opacity-50">
+                                    {message.Read ? 'Yes' : 'No'}
+                                    </div>
+                                </div>
+                                </div>
+                                    
+                                ))}
+                            
+                        </div>
                     </div>
-
-
                 </div>
             </main>
         </Layout>
     );
 };
 
-export default Messages;
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const res = await axios.get<Message[]>('http://localhost:5000/api/messages/all')
+    const messages = res.data
+    return { props: { messages } }
+}
+
+export default Messages
+
+
+/*
+<li >
+                                        <p>{message.Content}</p>
+                                        <p>Landlord ID: {message["Landlord ID"]}</p>
+                                        <p>Sent by Landlord: {message["Landlord Sent Message"] ? 'Yes' : 'No'}</p>
+                                        <p>Message ID: {message["Message ID"]}</p>
+                                        <p>Read: {message.Read ? 'Yes' : 'No'}</p>
+                                        <p>Sent Date: {message["Send Date"]}</p>
+                                        <p>Tenant ID: {message["Tenant ID"]}</p>
+
+                                    </li>
+                                    */
