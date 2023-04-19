07a345fa0496c2b731aca947214a4db2e62363f9
César Amaro <36521013+cesaramaro@users.noreply.github.com>
2023-04-19 18:39:31 -0400

    Login & SignUp components

---
+++ frontend/components/Login.tsx
@@ -0,0 +1,44 @@
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+const { name } = publicRuntimeConfig.site;
+
+const Login = () => {
+	const handleSubmit = async (event) => {};
+	return (
+		<div className='text-center'>
+			<input type='checkbox' id='login-modal' className='modal-toggle' />
+			<label htmlFor='login-modal' className='modal cursor-pointer'>
+				<label className='modal-box relative flex' htmlFor=''>
+					<div className='flex-col flex-none w-10 -my-6 bg-gradient-to-b pl-5 -ml-8 mr-5 from-primary to-accent overscroll-none'>
+						<br></br>
+					</div>
+
+					<div className='flex-col flex-auto'>
+						<label htmlFor='login-modal' className='btn btn-sm btn-circle absolute right-2 top-2'>
+							✕
+						</label>
+
+						<h3 className='text-4xl text-accent font-bold my-10'>Login</h3>
+
+						<form onSubmit={handleSubmit} className='p-5 form-control gap-10'>
+							<input type='text' id='first' name='first' placeholder='E-mail' className='bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+							<input type='text' id='last' name='last' placeholder='Password' className='bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+							<button type='submit' className='btn text-white bg-accent hover:bg-accent'>
+								Log In
+							</button>
+						</form>
+						<p className='pb-16'>
+							Don't have an account?{' '}
+							<a className='text-accent' href=''>
+								Register
+							</a>
+						</p>
+					</div>
+				</label>
+			</label>
+		</div>
+	);
+};
+
+export default Login;
---
+++ frontend/components/Signup.tsx
@@ -0,0 +1,69 @@
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+const { name } = publicRuntimeConfig.site;
+
+const Signup = () => {
+	const handleSubmit = async (event) => {};
+	return (
+		<div className='text-center'>
+			<input type='checkbox' id='signup-modal' className='modal-toggle' />
+			<label htmlFor='signup-modal' className='modal cursor-pointer'>
+				<label className='modal-box relative flex' htmlFor=''>
+					<div className='flex-col flex-none w-10 -my-6 bg-gradient-to-b pl-5 -ml-8 mr-5 from-primary to-accent overscroll-none'>
+						<br></br>
+					</div>
+
+					<div className='flex-col flex-auto'>
+						<label htmlFor='signup-modal' className='btn btn-sm btn-circle absolute right-2 top-2'>
+							✕
+						</label>
+
+						<h3 className='text-4xl text-accent font-bold my-10'>Create an Account</h3>
+
+						<form onSubmit={handleSubmit} className='p-5 form-control gap-10 flex'>
+							<div className='gap-5 flex-row flex'>
+								<input type='text' id='first' name='first' placeholder='First Name' className='w-1/2 bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+								<input type='text' id='last' name='last' placeholder='Last Name' className='w-1/2 bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+							</div>
+							<input type='text' id='email' name='email' placeholder='E-mail' className='bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+							<div className='gap-5 flex-row flex'>
+								<input type='text' id='password' name='password' placeholder='Password' className='w-1/2 bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+								<input type='text' id='cpassword' name='cpassword' placeholder='Confirm Password' className='w-1/2 bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+							</div>
+							<div className='text-left'>
+								<div className='flex-row pb-5'>Account Type</div>
+								<div className='flex-row text-left justify-start'>
+									<div className='mb-[0.125rem] mr-4 inline-block min-h-[1.5rem]'>
+										<input type='radio' name='inlineRadioOptions' className=' hover:cursor-pointer text-accent bg-gray-200 border-gray-200 focus:accent' id='inlineRadio1' value='option1' />
+										<label className='mt-px inline-block pl-2 hover:cursor-pointer' htmlFor='inlineRadio1'>
+											Student
+										</label>
+									</div>
+									<div className='mb-[0.125rem] mr-4 inline-block min-h-[1.5rem] pl-[1.5rem] hover:cursor-pointer'>
+										<input type='radio' name='inlineRadioOptions' className='hover:cursor-pointer  text-accent bg-gray-200 border-gray-200 focus:accent' id='inlineRadio2' value='option2' />
+										<label className='mt-px inline-block pl-2 hover:cursor-pointer' htmlFor='inlineRadio2'>
+											Landlord
+										</label>
+									</div>
+								</div>
+							</div>
+
+							<button type='submit' className='btn text-white bg-accent hover:bg-accent'>
+								Create Account
+							</button>
+						</form>
+						<p className='pb-16'>
+							Already have an account?{' '}
+							<a className='text-accent' href=''>
+								Log In
+							</a>
+						</p>
+					</div>
+				</label>
+			</label>
+		</div>
+	);
+};
+
+export default Signup;
