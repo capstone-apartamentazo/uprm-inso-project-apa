4096f940abd6e0aafc82c948e7c37dc51404140e
carlostorres62 <carlos.torres62@upr.edu>
2023-04-26 04:22:46 -0400

    updated priv amenitites to praetorian standard

--- backend/handler/private_amenities.py
+++ backend/handler/private_amenities.py
@@ -1,65 +1,105 @@
 from flask import jsonify
+from psycopg2 import Error as pgerror
+from util.config import db, logger, landlord_guard as guard
 from dao.private_amenities import PrivateAmenities
+from dao.units import Units
+from dao.accommodations import Accommodations
+import flask_praetorian as praetorian
 
 class PrivateAmenitiesHandler:
   def __init__(self):
     self.amenities = PrivateAmenities()
-  
-  def dictionary(self, row):
-    data = {}
-    data['Private Amenities ID'] = row[0]
-    data['Electricity'] = row[1]
-    data['Water'] = row[2]
-    data['Internet'] = row[3]
-    data['Bed'] = row[4]
-    data['Microwave'] = row[5]
-    data['Air Conditioner'] = row[6]
-    data['Parking'] = row[7]
-    data['Balcony'] = row[7]
-    data['Unit ID'] = row[9]
-    return data
+    self.units = Units()
+    self.accommodations = Accommodations()
 
   def getAll(self):
-    daoAmenities = self.amenities.getAll()
-    if daoAmenities:
-      result = []
-      for row in daoAmenities:
-        result.append(self.dictionary(row))
-      return jsonify(result), 200
-    else:
-      return jsonify('Error Occured'), 405
+    try:
+      daoAmenities = self.amenities.getAll()
+      if daoAmenities:
+        return jsonify([row for row in daoAmenities])
+      else:
+        return jsonify('Empty List')
+    except (Exception, pgerror) as e:
+      db.rollback()
+      logger.exception(e)
+      return jsonify('Error Occured'), 400
 
   def getById(self, json):
-    daoAmenities = self.amenities.getById(json['priv_amenities_id'])
-    if daoAmenities:
-      return jsonify(self.dictionary(daoAmenities)), 200
-    else:
-      return jsonify('Private Amenities Not Found'), 405
+    try:
+      daoAmenities = self.amenities.getById(json['priv_amenities_id'])
+      if daoAmenities:
+        return jsonify(daoAmenities)
+      else:
+        return jsonify('Private Amenities Not Found')
+    except (Exception, pgerror) as e:
+      db.rollback()
+      logger.exception(e)
+      return jsonify('Error Occured'), 400
 
   def getByUnitId(self, json):
-    daoAmenities = self.amenities.getByUnitId(json['unit_id'])
-    if daoAmenities:
-      result = []
-      for row in daoAmenities:
-        result.append(self.dictionary(row))
-      return jsonify(result), 200
-    else:
-      return jsonify('Private Amenities Not Found'), 405
+    try:
+      daoAmenities = self.amenities.getByUnitId(json['unit_id'])
+      if daoAmenities:
+        return jsonify(daoAmenities)
+      else:
+        return jsonify('Private Amenities Not Found')
+    except (Exception, pgerror) as e:
+      db.rollback()
+      logger.exception(e)
+      return jsonify('Error Occured'), 400
 
+  @praetorian.auth_required
   def addPrivateAmenities(self, json):
-    daoAmenities = self.amenities.addPrivateAmenities(json['unit_id'])
-    if daoAmenities:
-      return jsonify(self.dictionary(daoAmenities)), 200
-    else:
-      return jsonify('Error adding Private Amenities'), 405
+    try:
+      unit_id = json['unit_id']
+      valid, reason = self.checkUnit(unit_id)
+      if not valid:
+        return jsonify(reason)
+      daoAmenities = self.amenities.addPrivateAmenities(unit_id)
+      if daoAmenities:
+        return jsonify(daoAmenities)
+      else:
+        return jsonify('Error adding Private Amenities'), 400
+    except (Exception, pgerror) as e:
+      db.rollback()
+      logger.exception(e)
+      return jsonify('Error Occured'), 400
 
+  @praetorian.auth_required
   def updatePrivateAmenities(self, json):
-    identifier = json['priv_amenities_id']
-    electricity, water, internet = json['electricity'], json['water'], json['internet']
-    bed, microwave, air_conditioner = json['bed'], json['microwave'], json['air_conditioner']
-    parking, balcony = json['parking'], json['balcony']
-    updatedAmenities = self.amenities.updatePrivateAmenities(identifier, electricity, water, internet, bed, microwave, air_conditioner, parking, balcony)
-    if updatedAmenities:
-      return jsonify(self.dictionary(updatedAmenities)), 200
+    try:
+      amenities_id = json['priv_amenities_id']
+      electricity, water, internet = json['electricity'], json['water'], json['internet']
+      bed, microwave, air_conditioner = json['bed'], json['microwave'], json['air_conditioner']
+      parking, balcony = json['parking'], json['balcony']
+      valid, reason = self.checkAmenities(amenities_id)
+      if not valid:
+        return jsonify(reason)
+      updatedAmenities = self.amenities.updatePrivateAmenities(amenities_id, electricity, water, internet, bed, microwave, air_conditioner, parking, balcony)
+      if updatedAmenities:
+        return jsonify(updatedAmenities)
+      else:
+        return jsonify('Error updating Private Amenities'), 400
+    except (Exception, pgerror) as e:
+      db.rollback()
+      logger.exception(e)
+      return jsonify('Error Occured'), 400
+
+  def checkAmenities(self, identifier):
+    daoAmenities = self.amenities.getById(identifier)
+    if not daoAmenities:
+      return False, 'Shared Amenities Not Found'
+    return self.checkUnit(daoAmenities['unit_id'])
+
+  def checkUnit(self, identifier):
+    role = praetorian.current_rolenames().pop()
+    daoUnit = self.units.getById(identifier)
+    if not daoUnit:
+      return False, 'Unit Not Found'
+    daoAccommodation = self.accommodations.getById(daoUnit['accm_id'])
+    if not daoAccommodation:
+      return False, 'Accommodation Not Found'
+    if daoAccommodation['landlord_id'] != praetorian.current_user_id() or role != 'landlord':
+      return False, 'Accommodation is not own by Landlord'
     else:
-      return jsonify('Error updating Private Amenities'), 500
+      return True , ''
