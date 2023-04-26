aaff181610db053415e2dade8dd3bba436dafcf3
Elliot Cardona <elliot.cardona@upr.edu>
2023-04-25 23:29:15 -0400

    Connected Signup to test api. Added phone form.

--- frontend/components/Signup.tsx
+++ frontend/components/Signup.tsx
@@ -6,36 +6,54 @@ const { name } = publicRuntimeConfig.site;
 const Signup = () => {
 	const handleSubmit = async (event) => {
 		event.preventDefault();
+		if (event.target.password.value == event.target.cpassword.value) {
 
-		const data = {
-			first: event.target.first.value,
-			last: event.target.last.value,
-			email: event.target.email.value,
-			password: event.target.password.value,
-			cpassword: event.target.cpassword.value,
-			type: event.target.inlineRadio1.checked ? event.target.inlineRadio1.value : event.target.inlineRadio2.value,
-		};
+			const data = {
+				tenant_name: event.target.first.value + " " + event.target.last.value,
+				tenant_email: event.target.email.value,
+				tenant_password: event.target.password.value,
+				tenant_phone:event.target.phone.value,
+			};
 
-		const JSONdata = JSON.stringify(data);
-		alert(`${JSONdata}`);
-		const endpoint = '/api/something';
+			const JSONdata = JSON.stringify(data);
+			alert(`${JSONdata}`);
+			var endpoint = 'http://127.0.0.1:5000/api/tenants/new';
+			if (event.target.inlineRadio2.checked) {
+				endpoint = 'http://127.0.0.1:5000/api/landlords/new';
+			}
 
-		const options = {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSONdata,
-		};
+			const options = {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSONdata,
+			};
 
-		const response = await fetch(endpoint, options);
-		const result = await response.json();
-
-		alert(`${result.data}`);
+			await fetch(endpoint, options)
+				.then((response) => {
+					//console.log(response);
+					if (!response.ok) {
+						console.log(response)
+						//alert("first error")
+						throw new Error("Signup unsuccessful");
+					} else {
+						alert('Signup Successful')
+						return response.json()
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+					alert(error);
+				});
+		}else{
+			alert('Passwords dont match!')
+		}
+		
 	};
 	return (
 		<div className='text-center'>
-			
+
 			<label id='signup-modal' className='modal cursor-pointer'>
 				<label className='modal-box relative flex' htmlFor=''>
 					<div className='flex-col flex-none w-10 -my-6 bg-gradient-to-b pl-5 -ml-8 mr-5 from-primary to-accent overscroll-none'>
@@ -50,6 +68,7 @@ const Signup = () => {
 						<h3 className='text-4xl text-accent font-bold my-10'>Create an Account</h3>
 
 						<form onSubmit={handleSubmit} className='p-5 form-control gap-10 flex'>
+
 							<div className='gap-5 flex-row flex'>
 								<input type='text' id='first' name='first' placeholder='First Name' className='w-1/2 bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
 								<input type='text' id='last' name='last' placeholder='Last Name' className='w-1/2 bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
@@ -59,6 +78,11 @@ const Signup = () => {
 								<input type='password' id='password' name='password' placeholder='Password' className='w-1/2 bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
 								<input type='password' id='cpassword' name='cpassword' placeholder='Confirm Password' className='w-1/2 bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
 							</div>
+							<input type='tel' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" id='phone' name='phone' placeholder='Phone Number' className='bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+
+
+
+
 							<div className='text-left'>
 								<div className='flex-row pb-5'>Account Type</div>
 								<div className='flex-row text-left justify-start'>
