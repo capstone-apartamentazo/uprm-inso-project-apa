88c6b6db9bacdf649a356f520e2abb3ce7876ec3
Elliot Cardona <elliot.cardona@upr.edu>
2023-04-25 22:25:16 -0400

    Added option to select login type

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -11,23 +11,29 @@ const { name } = publicRuntimeConfig.site;
 
 
 const Login = () => {
-	
+
 	const router = useRouter()
-	
+
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-		
-		
+
+
+
 		const data = {
 			"landlord_email": event.target.email.value,
 			"landlord_password": event.target.password.value,
-			"type": "landlord"
+			"landlord": event.target.landlord.checked,
 		};
+
+		
 		//console.log(data);
 		const JSONdata = JSON.stringify(data);
 		//console.log(JSONdata);
-		const endpoint = 'http://127.0.0.1:5000/api/landlords/login';
+		var endpoint = 'http://127.0.0.1:5000/api/tenants/login';
+		if(data.landlord){
+			endpoint = 'http://127.0.0.1:5000/api/landlords/login';
+		}
 
 		const options = {
 			method: 'POST',
@@ -36,45 +42,45 @@ const Login = () => {
 			},
 			body: JSONdata
 		};
-
+		console.log(data.landlord)
 		await fetch(endpoint, options)
-		.then((response) => {
-			//console.log(response);
-			if (!response.ok){
-				console.log(response)
-				//alert("first error")
-				throw new Error("Email or password not match!");
-			}else{
-				return response.json()
-			}
-		})
-			
-		.then(result =>{
-			//alert(result);
-			const objc = {"token":(result['access_token']), "type":data.type, 'id':5};
-			const stringified = JSON.stringify(objc)
-			localStorage.setItem("data",stringified)
+			.then((response) => {
+				//console.log(response);
+				if (!response.ok) {
+					console.log(response)
+					//alert("first error")
+					throw new Error("Email or password not match!");
+				} else {
+					return response.json()
+				}
+			})
 
-			// localStorage.setItem('token', result["access_token"]);
-			// localStorage.setItem('type', data.type)
-			// localStorage.setItem('id', 1)
-			//console.log()
-			//console.log(JSON.parse(localStorage.getItem('data')!).token === ("Bearer "+result['access_token']));
-			alert(`Logged in success`);
-			//router.push('#');
+			.then(result => {
+				//alert(result);
+				const objc = { "token": (result['access_token']), "type": data.landlord, 'id': 5 };
+				const stringified = JSON.stringify(objc)
+				localStorage.setItem("data", stringified)
+				
+				// localStorage.setItem('token', result["access_token"]);
+				// localStorage.setItem('type', data.type)
+				// localStorage.setItem('id', 1)
+				//console.log()
+				//console.log(JSON.parse(localStorage.getItem('data')!).token === ("Bearer "+result['access_token']));
+				alert(`Logged in success`);
+				//router.push('#');
 
-			
 
-		})
-		.catch((error) => {
-			console.log(error);
-			alert(error);
-		  });
-		  
+
+			})
+			.catch((error) => {
+				console.log(error);
+				alert(error);
+			});
+
 	};
 	return (
-		<div className='text-center'>
-			
+		<div className='text-center '>
+
 			<label id='login-modal' className='modal cursor-pointer'>
 				<label className='modal-box relative flex' htmlFor=''>
 					<div className='flex-col flex-none w-10 -my-6 bg-gradient-to-b pl-5 -ml-8 mr-5 from-primary to-accent overscroll-none'>
@@ -88,9 +94,22 @@ const Login = () => {
 
 						<h3 className='text-4xl text-accent font-bold my-10'>Login</h3>
 
-						<form onSubmit={handleSubmit}  className='p-5 form-control gap-10'>
+						<form onSubmit={handleSubmit} className='p-5 form-control gap-10 '>
 							<input type='text' id='email' name='email' placeholder='E-mail' className='bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
 							<input type='password' id='password' name='password' placeholder='Password' className='bg-gray-200 bg-opacity-50 border border-transparent focus:ring-accent focus:border-accent pl-4' required />
+							<div className='text-left'>
+								<div className='flex-row pb-5'>Account Type</div>
+								<div className='flex-row text-left justify-start'>
+									
+									<div className='mb-[0.125rem] mr-4 inline-block min-h-[1.5rem]  hover:cursor-pointer'>
+										<input type='checkbox' name='checkbox' className='hover:cursor-pointer  text-accent bg-gray-200 border-gray-200 focus:accent' id='landlord' value='landlord' />
+										<label className='mt-px inline-block pl-2 hover:cursor-pointer' htmlFor='landlord'>
+											Landlord
+										</label>
+									</div>
+								</div>
+							</div>
+
 							<button type='submit' className='btn text-white bg-accent hover:bg-accent'>
 								Log In
 							</button>
@@ -102,9 +121,9 @@ const Login = () => {
 							</a>
 						</p>
 					</div>
-				</label>
-			</label>
-		</div>
+				</label >
+			</label >
+		</div >
 	);
 };
 
