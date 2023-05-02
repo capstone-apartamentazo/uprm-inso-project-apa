dfa34723196523303df82c5a12c8ced05edb05b5
César Amaro <36521013+cesaramaro@users.noreply.github.com>
2023-05-02 14:29:07 -0400

    Working on Unit page

--- frontend/useListings.tsx
+++ frontend/useListings.tsx
@@ -1,6 +1,7 @@
 import useSWR from 'swr';
 
-const baseUrl = 'https://api.apartamentazo.com/api/';
+const baseUrl2 = 'https://api.apartamentazo.com/api/';
+const baseUrl = 'http://127.0.0.1:5000/api/';
 const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then((res) => res.json());
 
 export const useListings = (path: string) => {
