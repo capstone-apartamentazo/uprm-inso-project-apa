6e0be6a88c8bc07702ceba4cba72563e5c56c82f
Elliot Cardona <elliot.cardona@upr.edu>
2023-05-04 00:21:11 -0400

    changed unit button location

--- frontend/components/ProfileAccommodation.tsx
+++ frontend/components/ProfileAccommodation.tsx
@@ -24,6 +24,8 @@ const ProfileAccommodation: React.FC<Props> = ({ title, address, id }) => {
                             {address}
                         </div>
                         <div className='flex items-center gap-2 mx-2 mt-2'>
+                            <button className='btn ring-1 ring-accent text-accent hover:bg-white hover:shadow-md hover:ring-2 grow'>Units</button>
+
                             <button className='btn ring-1 ring-primary hover:bg-white hover:shadow-md  hover:ring-2  grow'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-primary">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
@@ -36,7 +38,6 @@ const ProfileAccommodation: React.FC<Props> = ({ title, address, id }) => {
                                 </svg>
 
                             </button>
-                            <button className='btn ring-1 ring-accent text-accent hover:bg-white hover:shadow-md hover:ring-2 grow'>Units</button>
                         </div>
 
                     </div>
