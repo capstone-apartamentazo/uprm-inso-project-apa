2d1ef642d0ec927f73f6b07ce3f871d5c0f6f6c9
Elliot Cardona <elliot.cardona@upr.edu>
2023-05-04 00:23:58 -0400

    added unit interface

---
+++ frontend/Unit.tsx
@@ -0,0 +1,11 @@
+export interface Unit{
+    "accm_id": number,
+        "available": boolean,
+        "contract_duration": number,
+        "date_available": string,
+        "deleted_flag": boolean,
+        "price": number,
+        "shared": boolean,
+        "unit_id": number,
+        "unit_number": string
+}
