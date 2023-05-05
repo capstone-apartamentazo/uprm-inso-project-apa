f268de9bd55e2e65dc216be603a3493199bee463
Elliot Cardona <elliot.cardona@upr.edu>
2023-05-05 04:07:36 -0400

    Added functionality to buttons

--- frontend/components/Unit.tsx
+++ frontend/components/Unit.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
 type Props = {
     tenant?:string|null,
     num?: string,
     status?: boolean,
     a_id?: number,
+    id:number
     
 };
 
-const Unit: React.FC<Props> = ({tenant, num, status, a_id }) => {
+const Unit: React.FC<Props> = ({tenant, num, status, a_id, id }) => {
     
     return (
         
@@ -22,12 +24,15 @@ const Unit: React.FC<Props> = ({tenant, num, status, a_id }) => {
                         <div className='flex items-center gap-2  mt-2'>
                             <button className='btn ring-1 ring-accent text-accent hover:bg-white hover:shadow-md hover:ring-2 grow'>Add Tenant</button>
 
-                            <button className='btn ring-1 ring-primary hover:bg-white hover:shadow-md  hover:ring-2  grow'>
+                            <Link href={{
+                                pathname: '/units/edit',
+                                query: {unitId:id} // the data
+                            }} className='btn ring-1 ring-primary hover:bg-white hover:shadow-md  hover:ring-2  grow'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-primary">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                 </svg>
 
-                            </button>
+                            </Link>
                             <button className='btn ring-1 ring-red-500 hover:bg-white hover:shadow-md  hover:ring-2 grow '>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-red-500">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
