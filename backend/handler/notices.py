e05e52d8728a906d9c0b939b9894be96779bb80e
DannyBoy <daniel.crespo1@upr.edu>
2023-05-05 05:59:10 -0400

    Creating notice now sends emails to tenants

--- backend/handler/notices.py
+++ backend/handler/notices.py
@@ -1,14 +1,16 @@
 from flask import jsonify
 from psycopg2 import Error as pgerror
-from util.config import db, logger, landlord_guard as guard
+from util.config import db, logger, mail, landlord_guard as guard
 from dao.notices import Notices
 from dao.accommodations import Accommodations
 import flask_praetorian as praetorian
+from dao.landlords import Landlords
 
 class NoticeHandler:
   def __init__(self):
     self.notices = Notices()
     self.accommodations = Accommodations()
+    self.landlord = Landlords()
 
   def getAll(self):
     try:
@@ -51,7 +53,18 @@ class NoticeHandler:
       if not valid:
         return jsonify(reason)
       daoNotice = self.notices.addNotice(json['notice_title'], json['notice_content'], accm_id)
+      landlord = self.landlord.getById(praetorian.current_user_id())
+      currentTenants = self.notices.getCurrentTenants(accm_id)
+      tenantList = []
+      for tenant in currentTenants:
+          tenantList.append(tenant['tenant_email'])
       if daoNotice:
+        mail.send_message(
+          sender= landlord['landlord_email'],
+          subject=json['notice_title'],
+          recipients=tenantList,
+          body= json['notice_content']
+        )
         db.commit()
         return jsonify(daoNotice)
       else:
