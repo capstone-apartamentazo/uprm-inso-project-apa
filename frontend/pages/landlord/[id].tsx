4605d3ccbef18bb73c0e50ca9242aa6822942a6f
Elliot Cardona <elliot.cardona@upr.edu>
2023-05-05 04:08:58 -0400

    started fixing other user landlord profile

--- frontend/pages/landlord/[id].tsx
+++ frontend/pages/landlord/[id].tsx
@@ -2,65 +2,76 @@ import Layout from '@/components/Layout';
 import { useRouter } from 'next/router';
 import getConfig from 'next/config';
 import useSWR, { mutate } from 'swr';
-import { use, useEffect, useState } from 'react'
+import {  useEffect, useState } from 'react'
 import axios from 'axios';
+import { Accm } from 'Accm';
+import Accommodation from '@/components/Accommodation';
 
 const { publicRuntimeConfig } = getConfig();
 const { url: host } = publicRuntimeConfig.site;
-const uname = 'Bruce Wayne';
-const rating = '4';
-const joined = '2022';
-const location = 'Moca, PR';
-const languages = ['Spanish', 'English'];
-const reviewCount = 2;
+
 
 const Landlord = () => {
 	const router = useRouter();
 	const { id } = router.query;
 	//const [landlordId, setLandlordId] = useState<any>(null)
-	//const [landlord,setLandlord] = useState({deleted_flag: false,landlord_email: "",landlord_id: 0,landlord_name: "",landlord_password: "",landlord_phone: "",landlord_rating: "0.0"})
-
+	const [landlord, setLandlord] = useState({ deleted_flag: false, landlord_email: "", landlord_id: 0, landlord_name: "", landlord_password: "", landlord_phone: "", landlord_rating: "0.0" })
+	const [landlordImg, setLandlordImg] = useState('/images/default.jpeg')
 	useEffect(() => {
-        try{
+		if (id != null) {
+			
+
+
+				axios.get(`${host}/api/landlords/${id}`)
+					.then(res => {
+						return res.data
+					})
+					.then(result => {
+						console.log(result)
+						setLandlord(result)
+					}).catch(err=>{
+						console.error(err)
+					})
+
+				axios.get(`${host}/api/images/landlord/${id}`)
+					.then(res =>{
+						return res.data
+					})
+					.then(result =>{
+						//console.log(result['resources'][0])
+						return result['resources'][0]
+					})
+					.then(result =>{
+						setLandlordImg(result['secure_url'])
+					}).catch(err=>console.error(err))
 
 			
-			axios.get(`${host}/api/landlords/${id}`)
-			.then(res=>{
-				return res.data
-			})
-			.then(result=>{
-				console.log(result)
-				//setLandlord(result)
-			})
-            
-        }catch(err){
-			console.error(err)
-        }
-    }, [id])
+		}
+	}, [id])
 
-	
 
-	const { data: accms, error: accmsError, isLoading: isLoadingAccms } = useSWR( `${host}/api/accommodations/landlord/${id}`, (url: string) => fetch(url).then(res => res.json()));
 
-    
+	const { data: accms, error: accmsError, isLoading: isLoadingAccms } = useSWR(`${host}/api/accommodations/landlord/${id}`, (url: string) => fetch(url).then(res => res.json()));
 
-    if (accmsError) {
-        return <h1>Error</h1>
-    }
-    if (isLoadingAccms) return (
-        <div>
-            <h1>Loading...</h1>
 
-        </div>
-    )
-    if (!accms || accms == 'Accommodations Not Found') {
-        return (
-            <div className='mt-3'>
 
-                <h1 className='font-normal text-xl text-black'>No properties listed.</h1>
-            </div>
-        )
-    }
+	if (accmsError) {
+		return <h1>Error</h1>
+	}
+	if (isLoadingAccms) return (
+		<div>
+			<h1>Loading...</h1>
+
+		</div>
+	)
+	if (!accms || accms == 'Accommodations Not Found') {
+		return (
+			<div className='mt-3'>
+
+				<h1 className='font-normal text-xl text-black'>No properties listed.</h1>
+			</div>
+		)
+	}
 
 
 
@@ -74,18 +85,18 @@ const Landlord = () => {
 	return (
 		<Layout>
 			<main className='flex flex-row flex-nowrap pt-24'>
-				<div className='flex  flex-col flex-initial basis-1/4 pt-10 pl-6 pr-6 '>
+				<div className='flex  flex-col flex-initial basis-1/6 pt-10 pl-6 pr-6 '>
 					<div className='block min-w-full rounded-xl pt-6 bg-white ring-stone-200 ring-1 shadow-lg dark:bg-neutral-700 items-center text-center'>
 						<div className=''>
 							<div className='avatar my-4 mx-10'>
 								<div className=' w-40 rounded-full ring ring-accent ring-offset-base-100 ring-offset-2 hover:ring-4 hover:shadow-lg'>
-									<a href='/'><img className='aspect-square' src='/images/user.png' /></a>
+									<a href='/'><img className='aspect-square' src={landlordImg} /></a>
 								</div>
 							</div>
 						</div>
 						<div className=' '>
-							<h5 className='mb-2 text-2xl pt-2 font-bold leading-tight text-neutral-800 dark:text-neutral-50'>{uname}</h5>
-							<p className=' font-semibold pb-6 text-neutral-600 dark:text-neutral-200 '>Rating: {}/5</p>
+							<h5 className='mb-2 text-2xl pt-2 font-bold leading-tight text-neutral-800 dark:text-neutral-50'>{landlord.landlord_name}</h5>
+							<p className=' font-semibold pb-6 text-neutral-600 dark:text-neutral-200 '>Rating: { landlord.landlord_rating}/5</p>
 						</div>
 					</div>
 					<div className='flex justify-center pt-4'>
@@ -93,28 +104,34 @@ const Landlord = () => {
 							<div className='p-6'>
 								<h5 className='mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50'>Details:</h5>
 								<ul>
-									<li className=' text-neutral-600 dark:text-neutral-200 '>Joined in {joined}</li>
-									<li className=' text-neutral-600 dark:text-neutral-200 '>Lives in {location}</li>
-									<li className=' text-neutral-600 dark:text-neutral-200 '>
-										Speaks {languages[0]}, {languages[1]}
-									</li>
+									<li className=' text-neutral-600 dark:text-neutral-200 '>Phone: {landlord.landlord_phone}</li>
+									
 								</ul>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className='flex flex-col flex-initial basis-1/4 pt-10 pl-4 w-9/12'>
+				<div className='flex flex-col flex-initial basis-5/6 pt-10 px-4'>
 					<div className='rounded-md p-6'>
 						<h1 className=' text-3xl font-bold text-left  '>
-							
+
 							<label className=''>
 								Accommodations
 							</label>
 						</h1>
-						
+
+					</div>
+					<div className='flex  gap-2 mr-2'>
+						{accms.map((accm: Accm) => (
+
+							<Accommodation title={accm.accm_title} address={accm.accm_street} features={accm.accm_city} price={'100'} href='/' />
+
+						))}
+
 					</div>
 
-					
+
+
 				</div>
 			</main>
 		</Layout>
