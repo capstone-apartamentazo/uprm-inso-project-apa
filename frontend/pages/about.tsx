b6c988af985d2ee73f6322696333a4e4dcee05f9
César Amaro <36521013+cesaramaro@users.noreply.github.com>
2023-05-07 02:44:54 -0400

    Fix navbar pic & update about

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -18,24 +18,34 @@ const About = () => {
 				<div>
 					<section className='text-start m-10 mx-40 mt-20'>
 						<h1 className='font-semibold text-3xl my-4'>What we do</h1>
-						<p className='font-semibold text-gray-500 leading-10'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
-							five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-							including versions of Lorem Ipsum.
-						</p>
+						<div className='font-semibold text-gray-500 leading-10 space-y-4'>
+							<p>
+								At Apartamentazo, we understand the difficulties that come with finding suitable and affordable apartments near college campuses, and the stress that comes with being a landlord trying to reach potential tenants. That's why we've developed a platform that simplifies
+								the apartment search process for both students and landlords alike.
+							</p>
+							<p>
+								Our platform allows landlords to easily create and manage listings, track inquiries and messages, and send notices to tenants. For students, our platform offers a user-friendly interface that allows them to search for apartments based on their specific criteria, and
+								easily communicate with their landlord.
+							</p>
+							<p>
+								At Apartamentazo, we believe in creating a better experience for both landlords and students, streamlining the rental process to make it as smooth and stress-free as possible. With our platform, we aim to connect landlords and students in a way that is convenient,
+								efficient, and reliable.
+							</p>
+						</div>
 					</section>
 				</div>
 				<div>
 					<section className='text-end m-10 mx-40'>
 						<h1 className='font-semibold text-3xl my-4'>Our Team</h1>
 						<p className='font-semibold text-gray-500 mt-4 leading-10'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.{' '}
+							We are a team of passionate individuals who are committed to simplifying the college apartment searching process. Our diverse backgrounds and experiences help us bring a fresh perspective to the industry, enabling us to create innovative solutions that meet the needs of
+							both landlords and students.
 						</p>
 						<div className='items-center flex justify-center mt-10 mb-28 gap-5'>
-							<Card title="César Amaro" description="Software Engineer" position="Front-end" picture="" />
-							<Card title="Elliot Cardona" description="Software Engineer" position="Front-end" picture="" />
-							<Card title="Daniel Crespo" description="Software Engineer" position="Back-end" picture="" />
-							<Card title="Carlos Torres" description="Software Engineer" position="Back-end" picture="" />
+							<Card title='César Amaro' description='Software Engineer' position='Front-end' picture='' />
+							<Card title='Elliot Cardona' description='Software Engineer' position='Front-end' picture='' />
+							<Card title='Daniel Crespo' description='Software Engineer' position='Back-end' picture='' />
+							<Card title='Carlos Torres' description='Software Engineer' position='Back-end' picture='' />
 						</div>
 					</section>
 				</div>
